7087097e2b08d787b2974b97bdce2ac9c56849c5
Estefany Ochoa Romero <tefaoch23@gmail.com>
2025-04-14 11:27:16 +0200

    [ADD] I added fields to estate_property.py

--- demo_estate/models/estate_property.py
+++ demo_estate/models/estate_property.py
@@ -4,5 +4,22 @@ class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Real Estate Property'
 
-    name = fields.Char(string="Title", required=True)
-    estate_property_field = fields.Char(string="Estate Property Field")
+    
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(string='Description')
+    postcode = fields.Char(string='Postcode')
+    date_availability = fields.Date(string='Available From', copy=False)
+    expected_price = fields.Float(string='Expected Price', required=True)
+    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
+    living_area = fields.Integer(string='Living Area (sqm)')
+    facades = fields.Integer(string='Number of Facades')
+    garage = fields.Boolean(string='Garage')
+    garden = fields.Boolean(string='Garden')
+    garden_area = fields.Integer(string='Garden Area')
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+    )
+
+   
