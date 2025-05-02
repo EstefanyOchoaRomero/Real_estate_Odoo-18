e0f5622a2a8f838533cbe27fabfe8eb8af3ba0be
Estefany Ochoa Romero <tefaoch23@gmail.com>
2025-05-02 12:58:31 +0200

    [Finish] Property list report

--- demo_estate/__manifest__.py
+++ demo_estate/__manifest__.py
@@ -14,7 +14,7 @@ You can register and view properties for sale or rent.
     'website': "https://www.yourcompany.com",
 
     'category': 'Real Estate',
-    'version': '18.0.1.0.4',
+    'version': '18.0.1.0.5',
 
     # Required modules for this one to work properly
     'depends': ['base', 'account'],
