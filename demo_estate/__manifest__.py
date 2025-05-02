a45dbe7983539f777f2de6b26be4b08923771289
Estefany Ochoa Romero <tefaoch23@gmail.com>
2025-05-02 10:29:34 +0200

    [Fix] problems in report print

--- demo_estate/__manifest__.py
+++ demo_estate/__manifest__.py
@@ -14,7 +14,7 @@ You can register and view properties for sale or rent.
     'website': "https://www.yourcompany.com",
 
     'category': 'Real Estate',
-    'version': '18.0.1.0.3',
+    'version': '18.0.1.0.4',
 
     # Required modules for this one to work properly
     'depends': ['base', 'account'],
@@ -25,14 +25,14 @@ You can register and view properties for sale or rent.
         'security/ir.model.access.csv',
 
         # Firts views
-        # 'views/estate_property_views.xml',
+        'views/estate_property_views.xml',
         'views/estate_menus.xml',
 
         # Later reports
         'report/estate_property_templates.xml',
 
         # Finally, the actions that use those templates
-        'report/estate_property_reports.xml',
+        'report/estate_property_report.xml',
 
     ],
 
