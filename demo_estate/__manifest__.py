1075ae3a451491ccacd2a5d29c192d943c2c9075
Estefany Ochoa Romero <tefaoch23@gmail.com>
2025-05-04 21:32:22 +0200

    [CLEAN] Fix indentation in report files

--- demo_estate/__manifest__.py
+++ demo_estate/__manifest__.py
@@ -14,7 +14,7 @@ You can register and view properties for sale or rent.
     'website': "https://www.yourcompany.com",
 
     'category': 'Real Estate',
-    'version': '18.0.1.0.8',
+    'version': '18.0.1.0.9',
 
     # Required modules for this one to work properly
     'depends': ['base', 'account'],
